06f7f69033fd34047a0a3914a1cf02669793ef72
ScottPhilipFalconer <47996668+ScottPhilipFalconer@users.noreply.github.com>
2019-04-02 11:42:33 +1100

    Update main.c

--- main.c
+++ main.c
@@ -2,5 +2,81 @@
 
 int main()
 {
+    
+    //will need to move code around, not final order
+    
+    /*--------------------pass mark--------------------*/
+    
+        /*---------rotation cypher---------*/
+        
+            //encryption
+                //scan text to encode
+                    //scanf function
+                    //user prompt
+                    //user prompt includes key?
+                    //read as string/char
+                    //if string will only be read until whitespace
+                    //assign to variable/string/char
+                //run encoding key provided
+                    //number of letters offset
+                    //create algorithm using key
+                    //insert scanned input into algorithm
+                    //store result
+                //print encoded text to console
+                    //printf function
+                    //print original input alongside encoded input?
+                    //print key?
+                
+            //decryption
+                //scan encoded text
+                    //scanf function
+                    //read as string/char
+                    //assign to variable/string/char
+                //run decoding algorithm provided
+                //print decoded text to console
+                
+        /*---------------------------------*/
+        
+    
+        /*---------substitution cypher---------*/
+        
+            //encryption
+                //scan text to encode
+                //run encoding algorithm provided
+                //print encoded text to console
+                
+            //decryption
+                //scan text to decode
+                //run decoding algorithm provided
+                //print decoded text to console
+                
+        /*-------------------------------------*/
+        
+    /*-------------------------------------------------*/
+    
+    
+    /*--------------------advanced--------------------*/
+    
+        /*---------rotation cypher---------*/
+        
+            //scan text to decode
+            //run decoding algorithm
+            //print decoded text to console
+            
+        /*---------------------------------*/
+        
+        /*---------substitution cypher---------*/
+        
+            //scan text to decode
+            //run decoding algorithm
+            //print decoded text to console
+            
+        /*-------------------------------------*/
+        
+    /*------------------------------------------------*/
+    
+    //A is ASCII code 65, Z is ASCII code 90
+    //a is ASCII code 97, z is ASCII code 122
+
     return 0;
 }
